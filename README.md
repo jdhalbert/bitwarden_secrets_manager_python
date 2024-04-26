d94a783e80126afc0160201c54308e1fc3bddc59
jdhalbert <jdhalbert@gmail.com>
2024-04-26 13:14:44 -0700

    v1.0.0
    - interface changes will break any existing implementations
    - black formatting
    - add full CRUD support for secrets within a project
    - add ability to access secrets fields other than values
    - specify that at least one secret is required to already exist in a project
    - specify and enforce that project cannot have duplicate key names
    - improve organization, type hinting, and docs
    - update readme
    - fix passing stderr through to logging and exceptions
    - update to newest CLI syntax
    - other minor changes

--- README.md
+++ README.md
@@ -2,23 +2,23 @@
 This is a personal **unofficial** project. I have no affiliation with Bitwarden. Use at your own risk. Issues and feedback are welcome.
 
 # Python wrapper for [Bitwarden Secrets Manager](https://bitwarden.com/help/secrets-manager-overview/) CLI
-This module contains the `BWS` class, which is a Python wrapper for the `bws` [CLI application](https://bitwarden.com/help/secrets-manager-cli/). The class allows users to retrieve secrets stored in a Bitwarden Secrets Manager project. The module uses `subprocess` to call the `bws` CLI. The `bws` CLI application must be [downloaded separately](https://github.com/bitwarden/sdk/releases) and already present on your system (ideally in a `PATH` directory).
+This module contains the `BWS` class, which is a Python wrapper for the `bws` [CLI application](https://bitwarden.com/help/secrets-manager-cli/). The `BWS` class allows users to retrieve secrets stored in a Bitwarden Secrets Manager project. The module uses `subprocess` to call the `bws` CLI. The `bws` CLI application (v.0.4.0+) must be [downloaded separately](https://github.com/bitwarden/sdk/releases) and already present on your system (ideally in a `PATH` directory). 
 
-You must also have opted-in to the Bitwarden Secrets Manager beta and have generated a project, secret(s) and service account.
+You must also have a Bitwarden Secrets Manager account with an existing project, secret(s) and machine account.
 
 # How to use the `BWS` class
 
 ## Install
 1. Activate your environment of choice.
-2. Download [bws_python](https://github.com/jdhalbert/bitwarden_secrets_manager_python/releases).
+2. Download [bitwarden_secrets_manager_python](https://github.com/jdhalbert/bitwarden_secrets_manager_python/releases).
 3. Navigate to the folder containing `pyproject.toml` and run `pip install ./`
 
 ## Import
 ```
-from bws_python import BWS
+from bitwarden_secrets_manager_python import BWS
 ```
 
-Optionally use `logging` to see useful information, especially if using in a Jupyter Notebook or troubleshooting:
+Optionally use `logging` to see useful information, especially if using in a Jupyter Notebook or troubleshooting. Secrets and keys will not be logged.
 ```import logging
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 ```
@@ -27,26 +27,88 @@ logging.basicConfig(format='%(message)s', level=logging.INFO)
 Initialize a `BWS` object with:
  - The project name as it appears in Bitwarden Secrets Manager.
  - If the BWS_ACCESS_TOKEN environment variable has not been set in your environment, provide the token as a string.
+    - Your machine account that your access token is for must have at least `Read` access to your project (allowing get-like operations only). `Read and write` access is necessary to use functionality that adds, updates, or deletes secrets.
  - By default, the class uses the `bws` or `bws.exe` application found in a `PATH` directory, but a direct path to the application can also be supplied.
+ - **Note: your project must already exist and contain at least one secret, otherwise initialization will fail.**
+ - **Another note: your project *cannot* have any duplicate key names.** Although Bitwarden Secrets Manager does support duplicate key names (and keys instead by `id`), the `BWS` class mostly abstracts the `id` field for ease of use, keying on the key name (`key`) instead. This class will not allow creation of duplicate key names when using its CRUD interfaces. Be careful not to break compatibility by adding duplicate key names via the CLI, web interface, or other tools.
 
 Example if `bws` is not in your `PATH` and a token is not set as an environment variable:
-```
+```python
 my_bws = BWS(project_name='my_project_name', bws_access_token='my_token', bws_path='path/to/bws')
 ```
 
 Example if `bws` is in your `PATH` and a token is set as an environment variable:
-```
+```python
 my_bws = BWS(project_name='my_project_name')
 ```
 
 Note that each `BWS` object corresponds to a single project and service account. If you have multiple projects and/or service accounts to access, create separate `BWS` objects for each one.
 
-## Accessing individual secrets
-Access secrets from the BWS object as a key/value dictionary.
+### Note on Caching Behavior
+Upon initialization, all of the secrets in the given project are cached in the `BWS` instance. After initialization, get-like operations will read from the cache. Adds, updates, and deletes will update in your Bitwarden Secrets Manager account and incrementally update the cache, keeping the cache and online account in sync without unnecessary traffic. **Out-of-bound** changes to secrets made after initialization will not be reflected in the cache unless `refresh_secrets_cache()` is called on the instance.
+
+## Interacting with secrets
+Get a secret value:
+```python
+secret_value = my_bws['secret_key']
+# or
+secret_value = my_bws.get_secret('secret_key')['value']
+# or
+secret_value = my_bws.get_secret('secret_key', value_only=True)
 ```
-secret_value = my_bws['key']
-all_secrets = my_bws.items()
+
+Add a secret:
+```python
+my_bws['secret_key'] = 'secret_value' # same as updating
+# or
+my_bws.add_secret('secret_key', 'secret_value')
+```
+
+Update a secret:
+```python
+my_bws['secret_key'] = 'secret_value' # same as adding
+# or
+my_bws.update_secret_value('secret_key', 'secret_value')
+```
+
+Delete a secret:
+```python
+del my_bws['secret_key']
+# or
+my_bws.delete_secret('secret_key')
+```
+
+Get all secrets as a dictionary keyed by `key`:
+```python
+my_bws.as_dict()
+```
+
+Get all secrets as a list of tuples:
+```python
+my_bws.items()
+```
+
+Get number of secrets:
+```python
+len(my_bws)
+```
+
+Check if a secret exists:
+```python
+'secret_key' in my_bws
+```
+
+Other arbitrary calls to BWS CLI using your access token (but not your project name):
+```python
+my_bws.call_and_return_text(cl_args=['project', 'get', bws.PROJECT_ID], print_to_console=True, as_json=True)
 ```
 
 ## Other functions
-See docstrings in `bws.py` for other functionality.
+
+```python
+# use for making sure the CLI is working
+my_bws.help()
+my_bws.version()
+```
+
+See `bws.py` for more information.
--- bitwarden_secrets_manager_python/bws.py
+++ bitwarden_secrets_manager_python/bws.py
@@ -7,48 +7,75 @@ import json
 import logging
 import subprocess
 from pathlib import Path
+from copy import deepcopy
 
 logger = logging.getLogger(__name__)
 
+
 class BWS:
-    """ Bitwarden Secrets Manager Python wrapper. """
+    """Bitwarden Secrets Manager Python wrapper."""
 
-    _BWS_APPLICATION_PATH:Path
-    _BWS_ACCESS_TOKEN:str
-    _PROJECT_NAME:str
-    _PROJECT_ID:str
-    _SECRETS:dict
+    _BWS_APPLICATION_PATH: Path
+    _BWS_ACCESS_TOKEN: str
+    _PROJECT_NAME: str
 
-    def __init__(self, project_name:str, bws_access_token:str=None, bws_path:str='bws') -> None:
-        """ Initialize.
+    PROJECT_ID: str
+
+    _secrets: dict
+
+    def __init__(self, project_name: str, bws_access_token: str = None, bws_path: str = "bws") -> None:
+        """Initialize. Note: the given project MUST contain at least one secret.
 
         Args:
             project_name (str): Name of the project to get secrets for. The bws_access_token must have access
-                to this project. To include secrets for multiple projects, use separate objects. 
+                to this project. To include secrets for multiple projects, use separate objects.
             bws_access_token (str, optional): If not provided, will check if the environment variable is set and use
                 that. Supplied in CLI calls with the `-t` option. Defaults to None.
             bws_path (str, optional): Path to call `bws` or `bws.exe` application. Defaults to 'bws', which works if
                 the parent directory is in your system's PATH variable.
         """
-        self._set_token(bws_access_token=bws_access_token)
-        self._BWS_APPLICATION_PATH = Path(bws_path)
         self._PROJECT_NAME = project_name
-        self._PROJECT_ID = self._get_project_id()
-        self._SECRETS = self._get_secrets_from_bws()
+        self._BWS_APPLICATION_PATH = Path(bws_path)
+        self._set_token(bws_access_token=bws_access_token)
+        self.PROJECT_ID = self._get_project_id()
+        self.refresh_secrets_cache()
 
-        logger.info(f'Using Bitwarden Secrets Manager application at: {bws_path}')
+        logger.info(f"Using Bitwarden Secrets Manager application at: {bws_path}")
 
-    def __getitem__(self, key):
-        return self._SECRETS[key]
+    def __getitem__(self, key: str) -> str:
+        """When accessing the BWS object like a dictionary, only the 'value' field for the given key is returned.
 
-    def __len__(self):
-        return len(self._SECRETS)
+        Args:
+            key (str): Secret name.
 
-    def __contains__(self, item):
-        return item in self._SECRETS
+        Returns:
+            str: Secret 'value' field.
+        """
+        return self.get_secret(key=key, value_only=True)
 
-    def _set_token(self, bws_access_token:str|None) -> None:
-        """ If a bws_access_token is not provided, check is one is already set as an environment variable.
+    def __setitem__(self, key: str, value: str) -> None:
+        """Adds or updates the provided key/value pair.
+
+        Args:
+            key (str): Secret name.
+            value (str): Secret value.
+        """
+        if key in self:
+            self.update_secret_value(key=key, value=value)
+        else:
+            self.add_secret(key=key, value=value)
+
+    def __len__(self) -> int:
+        return len(self._secrets)
+
+    def __contains__(self, key) -> bool:
+        return key in self._secrets
+
+    def __delitem__(self, key) -> None:
+        self.delete_secret(key=key)
+
+    def _set_token(self, bws_access_token: str | None) -> None:
+        """If a bws_access_token is not provided, check is one is already set as an environment variable.
 
         Args:
             bws_access_token (str | None): Provide if not already set as BWS_ACCESS_TOKEN environment variable.
@@ -58,14 +85,17 @@ class BWS:
         """
         if bws_access_token:
             self._TOKEN = bws_access_token
-        elif 'BWS_ACCESS_TOKEN' in os.environ:
-            logger.info('Using BWS_ACCESS_TOKEN already set as environment variable.')
-            self._TOKEN = os.environ['BWS_ACCESS_TOKEN']
+        elif "BWS_ACCESS_TOKEN" in os.environ:
+            logger.info("Using BWS_ACCESS_TOKEN already set as environment variable.")
+            self._TOKEN = os.environ["BWS_ACCESS_TOKEN"]
         else:
-            raise ValueError('No BWS_ACCESS_TOKEN provided or set as environment variable.')
+            raise ValueError("No BWS_ACCESS_TOKEN provided or set as environment variable.")
 
-    def _get_project_id(self) -> str:
-        """ Gets the project's ID from the project name provided in the constructor.
+    def _get_project_id(self, print_to_console: bool = False) -> str:
+        """Gets the project's ID from the project name provided in the constructor.
+
+        Args:
+            print_to_console (bool, optional): Call print() on the stdout results. Defaults to False.
 
         Raises:
             ValueError: Project name not found.
@@ -73,76 +103,205 @@ class BWS:
         Returns:
             str: Project ID
         """
-        projects:list[dict] = self.call_and_return_text(cl_args=['list', 'projects'], #print_to_console=True,
-                                                        as_json=True)
+        projects: list[dict] = self.call_and_return_text(
+            cl_args=["project", "list"], print_to_console=print_to_console, as_json=True
+        )
         for project in projects:
-            if project['name'] == self._PROJECT_NAME:
-                return project['id']
-        raise ValueError(f'Project "{self._PROJECT_NAME}" not found. Choose from projects: '
-                         f'{[p["name"] for p in projects]}')
+            if project["name"] == self._PROJECT_NAME:
+                return project["id"]
+        raise ValueError(
+            f'Project "{self._PROJECT_NAME}" not found. Choose from projects: ' f'{[p["name"] for p in projects]}'
+        )
 
-    def _make_call(self, cl_args:list) -> str:
-        """ Make a call to `bws` with the provided args. Usually use self.call_and_return_text() instead of this.
+    def _make_call(self, cl_args: list[str], print_to_console: bool = False) -> str:
+        """Make a call to `bws` with the provided args. Usually use self.call_and_return_text() instead of this.
 
         Args:
-            cl_args (list): Args in list format (e.g. ['list', 'projects'])
+            cl_args (list[str]): Args in list format (e.g. ['project', 'list'])
+            print_to_console (bool, optional): Call print() on the stdout results. Defaults to False.
 
         Returns:
             str: Call results.
         """
         try:
-            return subprocess.check_output([self._BWS_APPLICATION_PATH] + cl_args + ['-c', 'no', '-t', self._TOKEN],
-                                           text=True)
+            output = subprocess.check_output(
+                [self._BWS_APPLICATION_PATH] + cl_args + ["-c", "no", "-t", self._TOKEN],
+                text=True,
+                stderr=subprocess.STDOUT,
+            )
+            if print_to_console:
+                print(output)
+            return output
+
         except subprocess.CalledProcessError as cpe:
-            logger.critical(f'CalledProcessError: {cpe.output}')
-            logger.critical('Note: access_token value redacted from list of commands in exception raised below:')
-            cpe.cmd.pop() # remove the access_token value
+            if print_to_console:
+                print(cpe.output)
+            logger.critical(
+                f"CalledProcessError: {cpe.output}. Note: access_token value redacted from list of"
+                "commands in exception raised below:"
+            )
+            cpe.cmd.pop()  # remove the access_token value so it doesn't get logged or displayed
             raise cpe
 
-    def _get_secrets_from_bws(self, full_detail:bool=False) -> list[dict] | dict[str,str]:
-        """ Get list of all secrets from `bws` CLI.
+    def _get_secrets_from_bws(self) -> dict[str, dict[str, str]]:
+        """Get list of all secrets from `bws` CLI.
 
         Returns:
-            list[dict] | dict[str,str]: List of "secret" objects if full_detail=True, else dict[key,value].
+            dict[str, dict[str, str]: Secert objects keyed by secret key name. E.g.:
+            {'secret_1':
+                {'id': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'organizationId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'projectId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'key': 'secret_1',
+                'value': 'secret_1_value',
+                'note': '',
+                'creationDate': '2001-01-01T01:23:45.678901234Z',
+                'revisionDate': '2001-01-01T01:23:45.678901234Z'},
+            ...
+            }
         """
-        secrets = self.call_and_return_text(cl_args=['list', 'secrets', self._PROJECT_ID], as_json=True)
-        return secrets if full_detail else {secret['key']: secret['value'] for secret in secrets}
+        secrets = self.call_and_return_text(cl_args=["secret", "list", self.PROJECT_ID], as_json=True)
+        secrets_keyed_by_key = {secret["key"]: secret for secret in secrets}
+        if len(secrets_keyed_by_key) != len(secrets):
+            raise ValueError(
+                "Projects with multiple keys with the same name are not supported. Each key name in your "
+                "project must be unique."
+            )
+        return secrets_keyed_by_key
 
-    def call_and_return_text(self, cl_args:list, print_to_console=False, as_json:bool=False) -> str|dict|list:
-        """ Make a call to `bws`.
+    def refresh_secrets_cache(self) -> None:
+        """Re-download all secrets from BWS."""
+        self._secrets = self._get_secrets_from_bws()
+
+    def call_and_return_text(
+        self, cl_args: list, print_to_console: bool = False, as_json: bool = True
+    ) -> str | dict | list:
+        """Make a call to `bws` CLI. Be careful with this as it can break compatibility with the BWS class (e.g.
+            deleting all secrets in a project or creating secrets with duplicate key names). See README.md for more
+            information.
 
         Args:
-            cl_args (list): Args in list format (e.g. ['list', 'projects']). Supply each word or option as a separate
+            cl_args (list): Args in list format (e.g. ['project', 'list']). Supply each word or option as a separate
                 list item.
             print_to_console (bool, optional): Call print() on the stdout results. Defaults to False.
-            as_json (bool, optional): Return the results converted to JSON. Defaults to False.
+            as_json (bool, optional): Return the results converted to JSON. Defaults to True.
 
         Returns:
             str|dict|list: stdout
         """
-        text:str = self._make_call(cl_args=cl_args)
-        if print_to_console:
-            print(text)
+        text: str = self._make_call(cl_args=cl_args, print_to_console=print_to_console)
         return json.loads(text) if as_json else text
 
-    def items(self) -> list[tuple[str,str]]:
-        """ Get all secret key/values in a dict-like items() call. 
+    def get_secret(self, key: str, value_only: bool = False) -> dict | str:
+        """When accessing the BWS object like a dictionary, only the 'value' field for the given key is returned.
+
+        Args:
+            key (str): Secret name.
+            value_only (bool): Return only the secret's 'value' field instead of the whole dict.
 
         Returns:
-            list[tuple[str,str]]: List of secrets like [(key, value)].
-        """
-        return self._SECRETS.items()
+            dict | str: Entire secret dict if value_only, else just the secret 'value' field as string.
+                E.g. if dict:
+                    {'id': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                    'organizationId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                    'projectId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                    'key': 'secret_1',
+                    'value': 'secret_1_value',
+                    'note': '',
+                    'creationDate': '2001-01-01T01:23:45.678901234Z',
+                    'revisionDate': '2001-01-01T01:23:45.678901234Z'}
 
-    def as_dict(self) -> dict[str,str]:
-        """ Get the internal _secrets dict containing all project secrets.
+                E.g. if str: 'secret_1_value'
+        """
+        return self._secrets[key]["value"] if value_only else self._secrets[key]
+
+    def add_secret(self, key: str, value: str, print_to_console: bool = False) -> dict:
+        """Add a new secret key/value pair. Adds to the internal cache without refreshing the whole thing.
+
+        Args:
+            key (str): Key to add.
+            value (str): Secret value.
+            print_to_console (bool, optional): Call print() on the stdout results. Defaults to False.
 
         Returns:
-            dict[str,str]: Internal _secrets object.
+            dict: Full new secret dict. E.g.
+                {'id': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'organizationId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'projectId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'key': 'secret_key',
+                'value': 'secret_value',
+                'note': '',
+                'creationDate': '2001-01-01T01:23:45.678901234Z',
+                'revisionDate': '2001-01-01T01:23:45.678901234Z'}
         """
-        return self._SECRETS
+        if key in self:
+            raise RuntimeError(f"Key {key} already exists in the project. Did you mean to call update_secret_value()?")
+        full_secret = self.call_and_return_text(
+            cl_args=["secret", "create", key, value, self.PROJECT_ID], print_to_console=print_to_console
+        )
+        logger.info(f'Added secret "{key}" to project "{self._PROJECT_NAME}"')
+        self._secrets[key] = full_secret
+        return full_secret
+
+    def update_secret_value(self, key: str, value: str, print_to_console: bool = False) -> dict:
+        """Update the value of a given secret. Updates internal cache without refreshing the whole thing.
+
+        Args:
+            key (str): Key to update.
+            value (str): Replacement value.
+            print_to_console (bool, optional): Call print() on the stdout results. Defaults to False.
+
+        Returns:
+            dict: Full updated secret dict. E.g.
+                {'id': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'organizationId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'projectId': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'key': 'secret_key',
+                'value': 'secret_value',
+                'note': '',
+                'creationDate': '2001-01-01T01:23:45.678901234Z',
+                'revisionDate': '2001-01-01T01:23:45.678901234Z'}
+        """
+        secret_id = self._secrets[key]["id"]
+        full_secret = self.call_and_return_text(
+            cl_args=["secret", "edit", secret_id, "--value", value], print_to_console=print_to_console
+        )
+        logger.info(f'Updated value for secret "{key}" in project "{self._PROJECT_NAME}"')
+        self._secrets[key] = full_secret
+        return full_secret
+
+    def delete_secret(self, key: str, print_to_console: bool = False) -> None:
+        """Delete a secret. Updates internal cache without refreshing the whole thing.
+
+        Args:
+            key (str): Key to delete.
+            print_to_console (bool, optional): Call print() on the stdout results. Defaults to False.
+        """
+        secret_id = self._secrets[key]["id"]
+        self.call_and_return_text(
+            cl_args=["secret", "delete", secret_id], print_to_console=print_to_console, as_json=False
+        )
+        del self._secrets[key]
+        logger.info(f'Deleted secret "{key}" from project "{self._PROJECT_NAME}"')
+
+    def items(self) -> list[tuple[str, dict]]:
+        """Call .items() on the secrets cache.
+
+        Returns:
+            list[tuple[str, dict]]: List of secrets like [(key:str, value:dict)].
+        """
+        return self._secrets.items()
+
+    def as_dict(self) -> dict[str, dict[str, dict]]:
+        """Returns a deepcopy of the internal secrets cache containing all project secrets.
+
+        Returns:
+            dict[str, dict[str, dict]]: Deepcopy of internal _secrets object.
+        """
+        return deepcopy(self._secrets)
 
     def help(self, print_to_console=True) -> str:
-        """ `bws` -h (help) command.
+        """`bws` -h (help) command.
 
         Args:
             print_to_console (bool, optional): Defaults to True.
@@ -150,10 +309,10 @@ class BWS:
         Returns:
             str: Console output as string.
         """
-        return self.call_and_return_text(cl_args=['-h'], print_to_console=print_to_console)
+        return self.call_and_return_text(cl_args=["-h"], print_to_console=print_to_console, as_json=False)
 
     def version(self, print_to_console=True) -> str:
-        """ `bws` -V (version) command.
+        """`bws` -V (version) command.
 
         Args:
             print_to_console (bool, optional): Defaults to True.
@@ -161,4 +320,4 @@ class BWS:
         Returns:
             str: Console output as string.
         """
-        return self.call_and_return_text(cl_args=['-V'], print_to_console=print_to_console)
+        return self.call_and_return_text(cl_args=["-V"], print_to_console=print_to_console, as_json=False)
